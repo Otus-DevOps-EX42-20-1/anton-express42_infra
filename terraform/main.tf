05afaa0bb161acaa9404f0b81080e17bae5b4246
anton-express42 <akurkin@express42.com>
2020-03-02 16:36:46 +0300

    HW#6

---
+++ terraform/main.tf
@@ -0,0 +1,67 @@
+terraform {
+  # Версия terraform
+  required_version = "0.12.21"
+}
+
+provider "google" {
+  # Версия провайдера
+  version = "2.15"
+  # ID проекта
+  project = var.project
+  region  = var.region
+}
+resource "google_compute_instance" "app" {
+  name         = "reddit-base-1583137359"
+  machine_type = "g1-small"
+  #самостоялка
+  zone = var.zone
+  #конец
+  tags = ["reddit-app"]
+  boot_disk {
+    initialize_params {
+      image = var.disk_image
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+  metadata = {
+    # путь до публичного ключа
+    ssh-keys = "appuser:${file("~/.ssh/appuser.pub")}"
+  }
+  connection {
+    type  = "ssh"
+    host  = self.network_interface[0].access_config[0].nat_ip
+    user  = "appuser"
+    agent = false
+    # путь до приватного ключа
+    private_key = file(var.private_key)
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+}
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+  # Название сети, в которой действует правило
+  network = "default"
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+  # Правило применимо для инстансов с перечисленными тэгами
+  target_tags = ["reddit-app"]
+}
+
+
+
